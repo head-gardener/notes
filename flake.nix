e7553ca4b5b9e109291767b362bbeea6e4734146
Head Gardener <trashbin2019np@gmail.com>
2024-12-30 00:25:36 +0300

    changes from ambrosia on Mon Dec 30 12:25:36 AM +03 2024

--- flake.nix
+++ flake.nix
@@ -9,19 +9,22 @@
       imports = [
 
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { pkgs, lib, ... }: {
         packages = rec {
           favicon_ico = pkgs.stdenvNoCC.mkDerivation {
             pname = "favicon-ico";
             version = "01072024";
 
-            src = ./static;
+            src = ./static/favicon.png;
+
+            dontUnpack = true;
 
             buildPhase = ''
-              ${pkgs.imagemagick}/bin/convert -resize 16x16 ./favicon.png ./16x16.png
-              ${pkgs.imagemagick}/bin/convert -resize 32x32 ./favicon.png ./32x32.png
-              ${pkgs.imagemagick}/bin/convert -resize 48x48 ./favicon.png ./48x48.png
+              ${pkgs.imagemagick}/bin/convert -resize 16x16 $src ./16x16.png
+              ${pkgs.imagemagick}/bin/convert -resize 32x32 $src ./32x32.png
+              ${pkgs.imagemagick}/bin/convert -resize 48x48 $src ./48x48.png
               ${pkgs.imagemagick}/bin/convert 16x16.png 32x32.png 48x48.png favicon.ico
             '';
 
@@ -39,7 +42,7 @@
 
           neorg-haskell-parser = let
             base = pkgs.haskellPackages.callPackage
-              (import static/neorg-haskell-parser.nix) {};
+              (import static/neorg-haskell-parser.nix) { };
           in lib.pipe base (with pkgs.haskell.lib; [
             doJailbreak
             dontCheck
@@ -47,7 +50,8 @@
             justStaticExecutables
           ]);
 
-          blog-render = with pkgs; (stdenvNoCC.mkDerivation rec {
+          blog-render = with pkgs;
+            (stdenvNoCC.mkDerivation rec {
               pname = "norg-render";
               version = "25122024";
 
@@ -59,31 +63,49 @@
                 "--highlight-style=pygments"
               ];
 
+              buildInputs = [
+                neorg-haskell-parser
+                pandoc
+              ];
+
               buildPhase = ''
                 mkdir build
                 find -type f -name '*.norg' | sed 's/.\/\(.*\)\.norg/\1/' | \
                   xargs -I {} sh -c '
-                    ${neorg-haskell-parser}/bin/neorg-pandoc -f "./{}.norg" | \
-                      ${lib.getExe pkgs.pandoc} \
-                      ${builtins.concatStringsSep " " pandocOpts} \
+                    sed "s/{:\([^:]*\):[^}]*}/{\.\/\1.html}[\1]/" -i "./{}.norg"; \
+                    neorg-pandoc -f "./{}.norg" | \
+                      pandoc ${builtins.concatStringsSep " " pandocOpts} \
                       --toc --from=json --metadata title="{}" -s -o "build/{}.html"; \
                     echo "- [{}](/{}.html)" >> index.md;
                   '
-                ${lib.getExe pkgs.pandoc} \
-                  ${builtins.concatStringsSep " " pandocOpts} \
+                pandoc ${builtins.concatStringsSep " " pandocOpts} \
                   --metadata title="Sitemap" -s index.md -o "build/index.html"; \
               '';
 
               installPhase = ''
                 cp build $out -rv
-                mkdir $out/static
+              '';
+            });
+
+            blog = pkgs.stdenvNoCC.mkDerivation {
+              pname = "blog";
+              version = "25122024";
+
+              src = ./unmanaged;
+
+              buildPhase = "";
+
+              installPhase = ''
+                mkdir $out
+                cp ${blog-render}/* $out/ -rv
                 cp ${favicon_ico} $out/favicon.ico -v
                 cp ${robots_txt} $out/robots.txt -v
+                mkdir $out/static
                 cp ${styles_css} $out/static/styles.css -v
               '';
-          });
+            };
         };
       };
-  flake = { };
-};
+      flake = { };
+    };
 }
